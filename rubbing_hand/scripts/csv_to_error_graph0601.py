af2e6049a6f2e9e8f9c86e0434afc2ff67a90c85
yoshiscythe <yoshiscythe@gmail.com>
2021-06-01 10:51:06 +0900

    kaeru

---
+++ rubbing_hand/scripts/csv_to_error_graph0601.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#coding: utf-8
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import MaxNLocator
+
+df_CAVS = pd.read_csv("/home/suzuki/ros_ws/ay_tools/fingervision/suzuki/rubbing_hand/data/0525/CAVS_error.csv")
+df_CAVS = df_CAVS.astype(float)
+# print(df_CAVS)
+# df_CAVS = df_CAVS[df_CAVS["id"]>=88]
+df_CAVS = df_CAVS.iloc[[65,68,71,74,77,80,83,86],:]
+
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 6))
+
+df_CAVS.plot(x="ratio", y="error", kind="scatter", ax=axes[0,0], sharex=True)
+df_CAVS.plot(x="ratio", y="mean", kind="scatter", ax=axes[0,1], sharex=True)
+df_CAVS.plot(x="ratio", y="std", kind="scatter", ax=axes[1,0], sharex=True)
+df_CAVS.plot(x="ratio", y="max_angular_velocity", kind="scatter", ax=axes[1,1], sharex=True)
+df_CAVS.plot(x="ratio", y="rms", kind="scatter", ax=axes[1,2], sharex=True)
+
+plt.show()
